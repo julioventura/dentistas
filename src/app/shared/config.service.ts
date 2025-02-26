0de63d25f2a9e72f5e73a799e0fdbe4a26c95709
julioventura <julioventura@gmail.com>
2025-02-26 01:14:45 -0300

    Versão 4.8

--- src/app/shared/config.service.ts
+++ src/app/shared/config.service.ts
@@ -10,7 +10,7 @@ export class ConfigService {
 
   constructor() {
     // Define aqui suas configurações
-    this.ambiente = 'Dentistas.com.br - Versão 4.6 (em 23/02/2025)'; // Ambiente atual 
+    this.ambiente = 'Dentistas.com.br - Versão 4.8 (em 26/02/2025)'; // Ambiente atual 
   }
 
 
