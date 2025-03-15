fed3c309612817af27645cfcd1394ddd31d4446b
julioventura <julioventura@gmail.com>
2025-03-15 02:36:45 -0300

    Versão 4.1.22 (de 15/03/2025)

--- src/app/shared/config.service.ts
+++ src/app/shared/config.service.ts
@@ -10,7 +10,7 @@ export class ConfigService {
 
   constructor() {
     // Define aqui suas configurações
-    this.ambiente = 'Versão 4.1.21 (de 14/03/2025)'; // Ambiente atual 
+    this.ambiente = 'Versão 4.1.22 (de 15/03/2025)'; // Ambiente atual 
   }
 
 
